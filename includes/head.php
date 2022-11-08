046e05dcea178225be1528c4c5de85a080e214a7
charley-mk <charlesmk792@gmail.com>
2022-11-08 12:13:02 +0300

    Add Jquery

--- includes/head.php
+++ includes/head.php
@@ -47,8 +47,8 @@
       background: url(images/loader-64x/Preloader_2.gif) center no-repeat #fff;
     }
   </style>
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+  <script src="../assets/js/jquery2.min.js"></script>
+  <script src="../assets/js/modernizr.js"></script>
   <script>
     $(window).load(function() {
       $(".se-pre-con").fadeOut("slow");
